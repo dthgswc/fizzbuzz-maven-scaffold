e7320cdcecc193a188f19ff592c15dc25795369a
紫川秀 <386221216@qq.com>
2020-01-15 16:40:12 +0800

    zc
    Change-Id: Ic12a475f21c0136ed086e80f238f177d05fac9ba
    Signed-off-by: 紫川秀 <386221216@qq.com>

--- src/main/java/GamN.java
+++ src/main/java/GamN.java
@@ -6,6 +6,7 @@ public class GamN {
 		this.rawN = i;
 	}
 
+	
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -8,6 +8,7 @@ public class Test1 {
 		Assert.assertThat(new GamN(i).toString(), is(s));
 	}
 
+	
 	@Test
 	public void ch() {
 		chN(15, "fizzbuzz");
