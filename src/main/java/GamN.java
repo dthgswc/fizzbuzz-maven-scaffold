8601768bce2695983e20a345523aac147cab09e1
紫川秀 <386221216@qq.com>
2020-01-15 16:03:00 +0800

    zc
    Change-Id: I57c74f2791608af980324887762e0cde1293028f
    Signed-off-by: 紫川秀 <386221216@qq.com>

--- src/main/java/GamN.java
+++ src/main/java/GamN.java
@@ -1,17 +1,19 @@
 
 public class GamN {
 	private int rawN;
-	public GamN(int i){
-		this.rawN=i;
+
+	public GamN(int i) {
+		this.rawN = i;
 	}
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		if(rawN%15==0)
+		if (rawN % 15 == 0)
 			return "fizzbuzz";
-		if(rawN%5==0)
+		if (rawN % 5 == 0)
 			return "buzz";
-		if(rawN%3==0)
+		if (rawN % 3 == 0)
 			return "fizz";
 		return String.valueOf(rawN);
 	}
--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,13 +2,14 @@ import static org.hamcrest.core.Is.is;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 public class Test1 {
-	public void chN(int i,String s){
+	public void chN(int i, String s) {
 		Assert.assertThat(new GamN(i).toString(), is(s));
 	}
+
 	@Test
-	public void ch()
-	{
+	public void ch() {
 		chN(15, "fizzbuzz");
 		chN(5, "buzz");
 		chN(3, "fizz");
